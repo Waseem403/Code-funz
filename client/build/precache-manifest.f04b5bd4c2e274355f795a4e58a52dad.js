08dba2d638d337cecada1b35038f62dc33f62a2b
Aleem Shaikh <b.waseem.403@gmail.com>
2020-02-16 18:28:20 +0530

    add files

---
+++ client/build/precache-manifest.f04b5bd4c2e274355f795a4e58a52dad.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "977c2e8c480428e79cc15cdffdda95ad",
+    "url": "/static/media/bg.977c2e8c.jpg"
+  },
+  {
+    "revision": "c934a8a6d299bfa55c48589e25982a8c",
+    "url": "/static/media/bg-masthead.c934a8a6.jpg"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "b454d72b5be1a89d66de",
+    "url": "/static/js/main.e7011667.chunk.js"
+  },
+  {
+    "revision": "09649911de47f953ff3b",
+    "url": "/static/js/2.83343717.chunk.js"
+  },
+  {
+    "revision": "b454d72b5be1a89d66de",
+    "url": "/static/css/main.07318036.chunk.css"
+  },
+  {
+    "revision": "09649911de47f953ff3b",
+    "url": "/static/css/2.f63c5cb6.chunk.css"
+  },
+  {
+    "revision": "a45caa9c75ba7d18c639ca7c37abdec0",
+    "url": "/index.html"
+  }
+];
